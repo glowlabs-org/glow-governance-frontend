b3b7e388f8f4cc5fe5f3d6ee42c6ee2c4d71c614
simon <simonnfts@gmail.com>
2023-12-18 00:53:37 -0500

    updating some process.env parsing

--- src/constants/api-url.ts
+++ src/constants/api-url.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const { NEXT_PUBLIC_GCA_API_URL } = process.env
+const NEXT_PUBLIC_GCA_API_URL = process.env.NEXT_PUBLIC_GCA_API_URL
 if (!NEXT_PUBLIC_GCA_API_URL) {
   throw new Error('NEXT_PUBLIC_GCA_API_URL is not set')
 }
--- src/constants/proxy-url.ts
+++ src/constants/proxy-url.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const { NEXT_PUBLIC_HTTP_PROXY_URL } = process.env
+const NEXT_PUBLIC_HTTP_PROXY_URL = process.env.NEXT_PUBLIC_HTTP_PROXY_URL
 if (!NEXT_PUBLIC_HTTP_PROXY_URL) {
   throw new Error('NEXT_PUBLIC_HTTP_PROXY_URL is not set')
 }
